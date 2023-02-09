a7eac45605303c9736e378d54698a7e1060e6e0d
imane ELKADI <123245005+imaneElkadi@users.noreply.github.com>
2023-02-09 21:31:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "hi imane";
+echo "connection databasee::";
+$servername='localhost';
+$usernamw='root';
+$psw='';
+$bd='gestEtudiant'
+$cnx= new mysqli($servername,$username,$psw,$bd);
+
+if($cnx->connect_error)
+  die("error: $cnx->connect_error");
+
 ?>
